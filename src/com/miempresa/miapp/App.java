da02f2ea43a1cd124c8b18e77ba73163df879ce5
estebanp22 <estebanpinedaardila2210@gmail.com>
2024-10-25 11:11:57 -0500

    ADD sumar y restar

---
+++ src/com/miempresa/miapp/App.java
@@ -0,0 +1,13 @@
+package com.miempresa.miapp;
+
+public class App {
+	
+	public static double sumar(int a, int b) {
+		return a+b;
+	}
+
+	public static double restar(int a, int b) {
+		return a-b;
+	}
+
+}
