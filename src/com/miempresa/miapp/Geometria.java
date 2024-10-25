c80891447a99845cad5592da5485fc3bed4a0a63
felipe lopez <cfelipe2005lv@gmail.com>
2024-10-25 11:38:37 -0500

    ADD areaCuadrado y areaTriangulo

---
+++ src/com/miempresa/miapp/Geometria.java
@@ -0,0 +1,18 @@
+package com.miempresa.miapp;
+
+public class Geometria {
+
+	/*
+	 * Metodo para calulcar el area de un cuadrado
+	 * @param ladoA
+	 * @param ladoB
+	 * @return ladoA*ladoB
+	 */
+	public static double areaCuadrardo(double ladoA, double ladoB) {
+		return ladoA*ladoB;
+	}
+	
+	public static double areaTriangulo(double base, double altura) {
+		return (base*altura)/2;
+	}
+}
