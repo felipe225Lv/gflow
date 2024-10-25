5409d84b10a33158288d5d676434fe4e8267733c
estebanp22 <estebanpinedaardila2210@gmail.com>
2024-10-25 11:14:06 -0500

    ADD multiplicar y dividir

--- src/com/miempresa/miapp/App.java
+++ src/com/miempresa/miapp/App.java
@@ -9,5 +9,13 @@ public class App {
 	public static double restar(int a, int b) {
 		return a-b;
 	}
+	
+	public static double multiplicar(int a, int b) {
+		return a*b;
+	}
+
+	public static double dividir(int a, int b) {
+		return a/b;
+	}
 
 }
